43243ddb38b8a64ac5001a9a4886aa263e1241ba
VinCoD <5895.2020@students.ku.ac.ke>
2022-11-01 16:39:36 +0300

    Added Colors

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,18 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text("NotePad"),
+        title:  Text("NotePad"),
         centerTitle: true,
         backgroundColor: Colors.blueGrey[600],
       ),
-      body: Center(
-        child: Text("Hello Ninjas",
-          style: TextStyle(
-            fontSize: 40,
+
+      body: const Center(
+        child:  Text("Hello Ninjas",
+          style:  TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold
           ),
         ),
-
-
 
       ),
         floatingActionButton: FloatingActionButton(onPressed: null,
